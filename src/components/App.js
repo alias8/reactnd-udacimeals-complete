a81eaaface8746ff6e568133e71198aee5cf325d
James Kirk <jameskirk8@gmail.com>
2017-08-17 08:14:40 +1000

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
@@ -6,8 +6,8 @@ class App extends Component {
       <div>
         Hello World
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
